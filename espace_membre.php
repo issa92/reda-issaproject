344639fb9975eff6d93a3989af70a92667be2b7d
issa <issalvaro9@gmail.com>
2020-04-02 17:02:18 +0200

    ajout du bon fichier

---
+++ espace_membre.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div align="center">
+        <h2> Inscription</h2>
+        <br> <br>
+        <form method="POST" aciton=""></form>
+            <table >
+                <tr>
+                    <td>
+                        <label for="pseudo">Pseudo</label>
+                </td>
+
+                    <td><input type="text"placeholder="votre pseudo" id="pseudo" name="pseudo" />
+                </td>
+            </tr>
+            <tr>
+                    <td align="right">
+                        <label for="mail">Mail</label>
+                </td>
+
+                    <td><input type="text"placeholder="votre mail" id="mail" name="mail" />
+                </td>
+            </tr>
+            <tr>
+                    <td align="right">
+                        <label for="mail2">confirmation du mail</label>
+                </td>
+
+                    <td><input type="email"placeholder="confirmer votre adresse mail" id="mail2" name="mail2" />
+                </td>
+            </tr>
+            <tr>
+                    <td align="right">
+                        <label for="mdp">confirmation du mail</label>
+                </td>
+
+                    <td><input type="password"placeholder="confirmer votre adresse mail" id="mail2" name="mail2" />
+                </td>
+            </tr
+        </table>
+
+        </form>
+    </div>    
+
+
+</body>
+</html>
